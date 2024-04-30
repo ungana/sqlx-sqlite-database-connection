a19d7edcdd68db31d32dafa662733d42937b4119
Joshua Hamilton <joshua@tinydog.io>
2024-04-30 19:49:56 -0400

    Logic for dummy data.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 use database::Database;
+use sqlx::{Error as SqlXError, SqlitePool};
 
 mod database;
+
+async fn add_user(pool: SqlitePool) -> Result<(), SqlXError> {
+    let query = "INSERT INTO Users (first_name, last_name, email) VALUES ($1, $2, $3)";
+
+    sqlx::query(query)
+        .bind("John")
+        .bind("Doe")
+        .bind("john.doe@example.com")
+        .execute(&pool)
+        .await?;
+
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() {
     match Database::connect().await {
-        Ok(_pool) => {
-            println!("Hello, world!");
-        }
+        Ok(pool) => match add_user(pool).await {
+            Ok(_) => println!("User created."),
+            Err(e) => panic!("Unable to create user: {}", e),
+        },
         Err(e) => panic!("Unable to connect to database: {}", e),
     }
 }
